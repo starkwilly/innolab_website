37f4e7808f99bc54500a33fa6f1b5f86f93d5f02
kevin.vega <kevin.vega@accenture.com>
2021-04-30 12:54:27 -0600

    temporary remove auth

--- src/layouts/Main/Main.js
+++ src/layouts/Main/Main.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import Header from '../../common/Header/Header';
 import Footer from '../../common/Footer/Footer';
-import { history } from '_helpers/history';
+// import { history } from '_helpers/history';
 
 import Container from 'react-bootstrap/Container';
 
 const Main = ({ children }) => {
-    const info = useSelector(state => state.auth.info);
+    /* const info = useSelector(state => state.auth.info);
 
     if (info === null) {
         history.push('/');
         return null;
-    }
+    } */
 
     return (
         <>
