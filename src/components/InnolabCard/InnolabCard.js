58a50496504dfecb69d3b0c0234fe761a0e8c48d
Manuel Cerdas <m.cerdas.salazar@accenture.com>
2021-10-08 10:24:32 -0600

    Adding modal with video player

--- src/components/InnolabCard/InnolabCard.js
+++ src/components/InnolabCard/InnolabCard.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import {React, useState} from 'react';
 import PropTypes from 'prop-types';
 import RenderMarkdown, { parseImgSrc } from '../../common/RenderMarkdown';
 import './InnolabCard.css';
 import ScrollAnimation from 'react-animate-on-scroll';
 import AccordionCard from '../AccordionCard/AccordionCard';
-import CardMediaContainer from '../CardMediaContainer/CardMediaContainer';
 import Container from '../../../node_modules/react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { Modal, Button } from 'react-bootstrap';
 
 const InnolabCard = (props) => {
     const {cardInfo, cardId, bgImage} = props;
     // window.log("InnolabCard", cardInfo, cardId);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
     return (
         <Container fluid="xs" >
             <Row className="mb-5">
@@ -32,11 +37,9 @@ const InnolabCard = (props) => {
                                         <h5 className="card-title text-white text-uppercase">{cardInfo.Title}</h5>
                                         <RenderMarkdown className="card-text">{cardInfo.BodyContent}</RenderMarkdown>
                                     </div>
-                                    {(cardInfo.Media.length > 0) && (
-
-                                            <CardMediaContainer mediaInfo={cardInfo.Media}/>
-                                        
-                                    )}
+                                    <Button variant="primary" onClick={handleShow}>
+                                        Launch demo modal
+                                    </Button>                                                                                                                                                
                                 </ScrollAnimation>
                             </Col>
                         </div>
@@ -47,6 +50,17 @@ const InnolabCard = (props) => {
                     </div>
                 </ScrollAnimation>
             </Row>
+
+            <Modal show={show} onHide={handleClose} centered size="lg" dialogClassName="modalWidth">                
+                <Modal.Body>
+                <iframe id="kmsembed-1_kzqrixnc" width="608" height="456" src="https://mediaexchange.accenture.com/embed/secure/iframe/entryId/1_kzqrixnc/uiConfId/27188232" className="kmsembed" allow="autoplay *; fullscreen *; encrypted-media *" referrerPolicy="no-referrer-when-downgrade"  frameBorder="0" title="Kaltura Player"></iframe>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>                
+                </Modal.Footer>
+            </Modal>
         </Container>
     )
 }
